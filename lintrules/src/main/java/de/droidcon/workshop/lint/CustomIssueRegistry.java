921636866ad45a01feb13d77421115c27b4f40a9
a11n <andre.diermann@gmail.com>
2015-05-31 13:39:16 +0200

    Simplified project structure and code.

--- lintrules/src/main/java/de/droidcon/workshop/lint/CustomIssueRegistry.java
+++ lintrules/src/main/java/de/droidcon/workshop/lint/CustomIssueRegistry.java
@@ -2,17 +2,11 @@ package de.droidcon.workshop.lint;
 
 import com.android.tools.lint.client.api.IssueRegistry;
 import com.android.tools.lint.detector.api.Issue;
-
 import java.util.Arrays;
 import java.util.List;
 
 public class CustomIssueRegistry extends IssueRegistry {
-  public CustomIssueRegistry() {
-  }
-
-  @Override
-  public List<Issue> getIssues() {
+  @Override public List<Issue> getIssues() {
     return Arrays.asList(HelloWorldDetector.ISSUE);
   }
-
 }
