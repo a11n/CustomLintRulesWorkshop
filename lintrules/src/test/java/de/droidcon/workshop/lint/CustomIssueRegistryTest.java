11f3378387c7af29d7de41b1924bdbde324f6543
a11n <andre.diermann@gmail.com>
2015-06-02 14:04:24 +0200

    Added tests and test resources.

---
+++ lintrules/src/test/java/de/droidcon/workshop/lint/CustomIssueRegistryTest.java
@@ -0,0 +1,24 @@
+package de.droidcon.workshop.lint;
+
+import com.android.tools.lint.detector.api.Issue;
+import de.droidcon.workshop.lint.detectors.advanced.HelloWorldDetector;
+import de.droidcon.workshop.lint.detectors.simple.IdPrefixDetector;
+import de.droidcon.workshop.lint.detectors.simple.StringReferencesDetector;
+import java.util.List;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CustomIssueRegistryTest {
+  @Test public void testGetIssues() throws Exception {
+    CustomIssueRegistry customIssueRegistry = new CustomIssueRegistry();
+
+    List<Issue> actual = customIssueRegistry.getIssues();
+
+    assertThat(actual).containsExactly(
+        HelloWorldDetector.ISSUE,
+        IdPrefixDetector.ISSUE,
+        StringReferencesDetector.ISSUE
+    );
+  }
+}
